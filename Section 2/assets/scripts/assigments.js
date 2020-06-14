52f993fdad39e2d382f62e50bb85a58c6ac45e12
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-06-14 19:22:15 +0200

    created new js file - assigments...

--- Section 2/assets/scripts/assigments.js
+++ Section 2/assets/scripts/assigments.js
@@ -1,8 +1,12 @@
-alert('Hello Joo');
+alert('Hello Assigments JS');
 
-// assignment 1
+/**
+ * 
+ *  Assignment 1
+ */
 
-/* Assignment - Variables , Operators, Data Types
+/* 
+    Assignment - Variables , Operators, Data Types
     1) Create two variables: One that holds a fictional user input (a number of your choice) and one that holds no value initially (e.g. "result").
     2) Set the result variable to 18 plus the value stored in user input.
     3) Add three additional lines of code where you change the result variable again (by subtracting a value of your choice, multiplying it and dividing it).
@@ -19,9 +23,9 @@ alert('Hello Joo');
             //  result = result / result;
             //  alert(result);
 
- /**
-  *  Shadowed Variables
-  **/ 
+/**
+ *  Shadowed Variables
+ **/ 
 
             // let userName = 'Milos';
 
@@ -34,33 +38,34 @@ alert('Hello Joo');
             // console.log(userName);
             // greetUser('Miciki');
 
-    //   It indeed is not allowed on the same level/ in the same scope.
+/*
+    It indeed is not allowed on the same level/ in the same scope.
 
-    // So this would fail:
-            // let userName = 'Max';
-            // let userName = 'Manu';
-  // Why does it work in the first code snippet though?
+    So this would fail:
+            let userName = 'Max';
+            let userName = 'Manu';
+    Why does it work in the first code snippet though?
 
-  // Because we first create a global variable userName via
+    Because we first create a global variable userName via
 
-  // let userName = 'Max';
-  // But then we never re-declare that on the global level (that would not be allowed).
+    let userName = 'Max';
+    But then we never re-declare that on the global level (that would not be allowed).
 
-  // We only declare another variable inside of the function. But since variables in functions get their own scope, JavaScript does something which is called "shadowing".
+    We only declare another variable inside of the function. But since variables in functions get their own scope, JavaScript does something which is called "shadowing".
 
-  // It creates a new variable on a different scope - this variables does not overwrite or remove the global variable by the way - both co-exist.
+    It creates a new variable on a different scope - this variables does not overwrite or remove the global variable by the way - both co-exist.
 
-  // When referring to userName inside of the greetUser function we now always refer to the local, shadowed variable. Only if no such local variable existed, JavaScript would fall back to the global variable.
+    When referring to userName inside of the greetUser function we now always refer to the local, shadowed variable. Only if no such local variable existed, JavaScript would fall back to the global variable.
+*/
+/**
+ * 
+ *  Assigment 2 - Function
+ */
 
-  /**
-   * 
-   *    Assigment - Function
-   */
+/*
+    1)
 
-   /*
-        1)
-
-   */
+*/
 
    function firstFunc(){
        alert('Gidra');
