960ecb25a2b89c6e4cec059f7b93c5fd1a1971ab
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-07-08 12:16:38 +0200

    Adding user choices to the game...

--- Section 6/app.js
+++ Section 6/app.js
@@ -1,18 +1,41 @@
+// const startGameBtn = document.getElementById('start-game-btn');
+
+// function startGame(){
+//     console.log('Game is starting...');
+// }
+
+// const person = {
+//     name: 'Milos',
+//     greet: function greet(){
+//         console.log('Hello there!');
+//     }
+// }
+
+// person.greet();
+
+// console.dir(startGame); // functions are Objects!
+
+// startGameBtn.addEventListener('click', startGame);
+
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame(){
-    console.log('Game is starting...');
-}
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
 
-const person = {
-    name: 'Milos',
-    greet: function greet(){
-        console.log('Hello there!');
+const getPlayerChoice = function(){
+    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}`, '').toUpperCase();
+
+    if(selection !== ROCK && selection !== PAPER && selection !== SCISSORS){
+        alert(`Invalid choice! W chose ${DEFAULT_USER_CHOICE} for you!`);
+        return DEFAULT_USER_CHOICE;
     }
+    return selection;
 }
 
-person.greet();
-
-console.dir(startGame); // functions are Objects!
-
-startGameBtn.addEventListener('click', startGame);
+startGameBtn.addEventListener('click', function(){
+    console.log('Game is starting!');
+    const playerSelection = getPlayerChoice();
+    console.log(playerSelection);
+});
