2216a66ba9b5ed81148cdc4d277b056b5981a687
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-07-10 11:22:37 +0200

    task 1 - Re-write the function as an arrow function...

--- Section 6/functions-assignment-problem/assignment.js
+++ Section 6/functions-assignment-problem/assignment.js
@@ -1,5 +1,12 @@
-function sayHello(name) {
+function sayHello1(name) {
   console.log('Hi ' + name);
 }
 
-sayHello();
+sayHello1('Miciki');
+
+// 1. Re-write the function as an arrow function
+
+const sayHello2 = (name) => console.log('Hi ' + name);
+
+sayHello2('Milos');
+
