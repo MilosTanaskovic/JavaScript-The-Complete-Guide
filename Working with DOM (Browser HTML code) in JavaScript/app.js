bd5316909ab4d9dac8a77614b06c9a5f1f847d35
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-07-15 13:57:19 +0200

    styling DOM elements...

--- Working with DOM (Browser HTML code) in JavaScript/app.js
+++ Working with DOM (Browser HTML code) in JavaScript/app.js
@@ -13,3 +13,26 @@ const listItemElements = document.getElementsByTagName('li');
 for(const listItemEl of listItemElements){
     console.dir(listItemEl);
 }
+
+
+const ul = document.body.firstElementChild.nextElementSibling;
+const firstLi = ul.firstElementChild;
+
+console.log(firstLi);
+
+const section = document.querySelector('section');
+const button = document.querySelector('button');
+
+// section.style.backgroundColor = 'blue';
+section.className = 'red-bg';
+
+button.addEventListener('click', () => {
+  // if (section.className === 'red-bg visible') {
+  //   section.className = 'red-bg invisible';
+  // } else {
+  //   section.className = 'red-bg visible';
+  // }
+
+  // section.classList.toggle('visible');
+  section.classList.toggle('invisible');
+});
