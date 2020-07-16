ad58dcca30117d68ca92cf22843d1adf87c76450
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-07-16 11:39:52 +0200

    Selecting the Modal and 'add button'...

---
+++ Working with DOM (Browser HTML code) in JavaScript/dom-project/assets/scripts/app.js
@@ -0,0 +1,5 @@
+const addMovieModal = document.getElementById('add-modal');
+const startAddMovieButton = document.querySelector('header button');
+
+console.log(addMovieModal);
+console.dir(startAddMovieButton);
