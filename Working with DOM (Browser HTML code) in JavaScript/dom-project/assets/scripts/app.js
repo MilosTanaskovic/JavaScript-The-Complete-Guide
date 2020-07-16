020778cd1153cf49fc9edebcb35922ebec322071
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-07-16 12:20:14 +0200

    controling the backdrop...

--- Working with DOM (Browser HTML code) in JavaScript/dom-project/assets/scripts/app.js
+++ Working with DOM (Browser HTML code) in JavaScript/dom-project/assets/scripts/app.js
@@ -3,13 +3,26 @@ const addMovieModal = document.getElementById('add-modal');
 // const addMovieModal = document.body.children[1];
 const startAddMovieButton = document.querySelector('header button');
 // const startAddMovieButton = document.querySelector('header').lastElementChild();
+const backdrop = document.getElementById('backdrop');
+const cancelButton = document.querySelector('#add-modal button.btn--passive');
 
 // console.log(addMovieModal);
 // console.dir(startAddMovieButton);
-
+const toggleBackdrop = () => {
+    backdrop.classList.toggle('visible');
+};
 const toggleMovieModal = () => { // function() {}
 
     addMovieModal.classList.toggle('visible');
+    toggleBackdrop();
 };
+const backdropClickHandler = () => {
+    toggleMovieModal();
+}
+const cancelAddMovie = () => {
+    toggleMovieModal();
+}
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelButton.addEventListener('click', cancelAddMovie);
