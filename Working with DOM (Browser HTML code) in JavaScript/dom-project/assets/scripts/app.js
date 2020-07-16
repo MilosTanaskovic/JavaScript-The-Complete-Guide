2084a5c15ea669466d3f7f825229908c344775d7
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-07-16 11:58:10 +0200

    Opening a modal by changing css classes

--- Working with DOM (Browser HTML code) in JavaScript/dom-project/assets/scripts/app.js
+++ Working with DOM (Browser HTML code) in JavaScript/dom-project/assets/scripts/app.js
@@ -1,5 +1,15 @@
 const addMovieModal = document.getElementById('add-modal');
+// const addMovieModal = document.querySelecor('#add-modal');
+// const addMovieModal = document.body.children[1];
 const startAddMovieButton = document.querySelector('header button');
+// const startAddMovieButton = document.querySelector('header').lastElementChild();
 
-console.log(addMovieModal);
-console.dir(startAddMovieButton);
+// console.log(addMovieModal);
+// console.dir(startAddMovieButton);
+
+const toggleMovieModal = () => { // function() {}
+
+    addMovieModal.classList.toggle('visible');
+};
+
+startAddMovieButton.addEventListener('click', toggleMovieModal);
