d3a3b09bc00e573395d0c90badff2a8b6def8d2d
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-07-20 08:12:24 +0200

    creating array...

---
+++ More on Array and Iterables/app.js
@@ -0,0 +1,13 @@
+const numbers = [1,2,3];
+
+const moreNumers = Array(5,2); //[5,2]
+const emptyLength = Array(5); // [empty of 5 elements]
+
+const yetMoreNumbers = Array.of(1,2); // [1,2]
+
+const arraylistItems = Array.form(1,2); // [1,2]
+const arraylistItems = Array.form('Hi!') // ['H', 'i', '!'];
+
+const listItems = document.querySelectorAll('li');
+
+const arraylistItems = Array.from(listItems);
