c320cd3b60586ce649d3cb7457bf6fcdb092d427
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-07-24 09:10:49 +0200

    added new methods - array...

--- More on Array and Iterables/app.js
+++ More on Array and Iterables/app.js
@@ -23,4 +23,20 @@ for(const data of analyticData){
         console.log(dataPoint);
     }
 }
-// add
+// push, unshift,pop, shift, splice...
+const hobbies = ['Sports', 'Cooking'];
+hobbies.push('Reading');
+hobbies.unshift('Coding');
+const poppedValue = hobbies.pop();
+hobbies.shift();
+console.log(hobbies);
+
+hobbies[1] = 'Coding';
+// hobbies[5] = 'Reading'; // rarely used
+console.log(hobbies, hobbies[4]);
+
+hobbies.splice(1, 0, 'Good Food');
+console.log(hobbies);
+
+const removedElements = hobbies.splice(-2, 1);
+console.log(hobbies);
