a76a54e768596803a847ae8b6b70be9eda001926
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-07-27 16:55:18 +0200

    sort() and reverse()...

--- More on Array and Iterables/app.js
+++ More on Array and Iterables/app.js
@@ -100,4 +100,17 @@ const taxAdjustedPrices = prices.map((price, idx, prices) => {
     return priceObj;
 });
 
-console.log(prices, taxAdjustedPrices);
+//console.log(prices, taxAdjustedPrices);
+
+/**
+ *  sort() ing and reverse() ing 
+ */
+
+const sortedPrices = prices.sort((a,b) => {
+    if(a > b) return 1;
+    else if(a === b) return 0;
+    else return -1;
+});
+
+console.log(sortedPrices);
+console.log(sortedPrices.reverse());
