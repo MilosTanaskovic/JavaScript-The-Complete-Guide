23a6aad4ef9af75c05a692477ec253fe838da99f
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-07-27 14:57:13 +0200

    alternative to for loops: forEach() method...

--- More on Array and Iterables/app.js
+++ More on Array and Iterables/app.js
@@ -1,58 +1,82 @@
-const numbers = [1,2,3];
+// const numbers = [1,2,3];
 
-const moreNumers = Array(5,2); //[5,2]
-const emptyLength = Array(5); // [empty of 5 elements]
+// const moreNumers = Array(5,2); //[5,2]
+// const emptyLength = Array(5); // [empty of 5 elements]
 
-const yetMoreNumbers = Array.of(1,2); // [1,2]
+// const yetMoreNumbers = Array.of(1,2); // [1,2]
 
-const arraylistItems = Array.form(1,2); // [1,2]
-const arraylistItems = Array.form('Hi!') // ['H', 'i', '!'];
+// const arraylistItems = Array.form(1,2); // [1,2]
+// const arraylistItems = Array.form('Hi!') // ['H', 'i', '!'];
 
-const listItems = document.querySelectorAll('li');
+// const listItems = document.querySelectorAll('li');
 
-const arraylistItems = Array.from(listItems);
+// const arraylistItems = Array.from(listItems);
 
-// storing data in array
+// // storing data in array
 
-const hobbies = ['Cooking', 'Sports'];
-const personalData = [30, 'Max', {moreDetail: []}];
+// const hobbies = ['Cooking', 'Sports'];
+// const personalData = [30, 'Max', {moreDetail: []}];
 
-const analyticData = [[1,1.2][2,2.3]];
-for(const data of analyticData){
-    for(const dataPoint of data){
-        console.log(dataPoint);
-    }
-}
-// push, unshift,pop, shift, splice...
-const hobbies = ['Sports', 'Cooking'];
-hobbies.push('Reading');
-hobbies.unshift('Coding');
-const poppedValue = hobbies.pop();
-hobbies.shift();
-console.log(hobbies);
+// const analyticData = [[1,1.2][2,2.3]];
+// for(const data of analyticData){
+//     for(const dataPoint of data){
+//         console.log(dataPoint);
+//     }
+// }
+// // push, unshift,pop, shift, splice...
+// const hobbies = ['Sports', 'Cooking'];
+// hobbies.push('Reading');
+// hobbies.unshift('Coding');
+// const poppedValue = hobbies.pop();
+// hobbies.shift();
+// console.log(hobbies);
 
-hobbies[1] = 'Coding';
-// hobbies[5] = 'Reading'; // rarely used
-console.log(hobbies, hobbies[4]);
+// hobbies[1] = 'Coding';
+// // hobbies[5] = 'Reading'; // rarely used
+// console.log(hobbies, hobbies[4]);
 
-hobbies.splice(1, 0, 'Good Food');
-console.log(hobbies);
+// hobbies.splice(1, 0, 'Good Food');
+// console.log(hobbies);
 
-const removedElements = hobbies.splice(-2, 1);
-console.log(hobbies);
+// const removedElements = hobbies.splice(-2, 1);
+// console.log(hobbies);
 
-// slice - copy values
+// // slice - copy values
 
-const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
-// slice()
-const storedResults = testResults.slice(); // we can add parameter in the slice(2) - select from 2 index
+// const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+// // slice()
+// const storedResults = testResults.slice(); // we can add parameter in the slice(2) - select from 2 index
 
-// concat()
-const storedResults = testResults.concat(1,2); // [1,5.3,1.5,10.99,-5,10,1,2];
+// // concat()
+// const storedResults = testResults.concat(1,2); // [1,5.3,1.5,10.99,-5,10,1,2];
 
-testResults.push(5.91); // [1, 5.3, 1.5, 10.99, -5, 10, 5.91]
+// testResults.push(5.91); // [1, 5.3, 1.5, 10.99, -5, 10, 5.91]
 
-console.log(storedResults, testResults);
-// indexOf(), lastIndexOf()
-console.log(testResults.indexOf(5.3)); // 1
-console.log(storedResults.lastIndexOf(1)); // 6
+// console.log(storedResults, testResults);
+// // indexOf(), lastIndexOf()
+// console.log(testResults.indexOf(5.3)); // 1
+// console.log(storedResults.lastIndexOf(1)); // 6
+
+//  const personData = [{name: 'Milos', id: 1}, {name:'Miciki', id: 2}];
+//  console.log(personData.indexOf({name: 'Milos'})); // it doesn't work with indexOf() method
+
+// // find() for Objects
+// const manuel = personData.find((person, idx, persons) => {
+//     return person.name === 'Milos';
+// });
+
+// console.log(manuel);
+
+const prices = [10.99, 5.99, 3.99, 6.59];
+const tax = 0.19;
+const taxtAdjustedPrices = [];
+
+// for(const price of prices){
+//     taxtAdjustedPrices.push(price * (1+ tax));
+// }
+
+prices.forEach((price, idx, prices) => {
+    const pricesObj = { index: idx, texAdjPrice: price * (1+ tax) };
+    taxtAdjustedPrices.push(pricesObj);
+});
+console.log(taxtAdjustedPrices);
