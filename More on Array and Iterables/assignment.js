c1111b0dad0680b75b117d84c244f20cbbcd1248
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-08-04 12:53:56 +0200

    assig 1.2 - map method...

--- More on Array and Iterables/assignment.js
+++ More on Array and Iterables/assignment.js
@@ -3,3 +3,6 @@ const number = [1,2,3,4,5,6];
 const numGreater5 = number.filter(val => val > 5);
 
 console.log(numGreater5);
+
+const mappedNumber = number.map(val => ({num: val}));
+console.log(mappedNumber);
