e3c837f4e9b7b5e35cfd31779dff1b3d926bcc9c
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-08-17 15:58:07 +0200

    shorthand property syntax...

--- More On Objects/assets/scripts/objects.js
+++ More On Objects/assets/scripts/objects.js
@@ -1,16 +1,47 @@
-const person = {
-    name: 'Milos',
-    age: 26,
-    hobbies: ['Sport', 'Cooking'],
-    greet: function(){
-        alert('Hi there!');
+// const person = {
+//     name: 'Milos',
+//     age: 26,
+//     hobbies: ['Sport', 'Cooking'],
+//     greet: function(){
+//         alert('Hi there!');
+//     }
+// };
+
+// // Adding, Modifying, & Deleting Properties
+// //person.age = 31;
+// delete person.age;
+// person.isAdmin = true;
+
+// console.log(person);
+// person.greet();
+
+const addMovieBtn = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
+
+const movies = [];
+
+const addMovieHandler = () => {
+    const title = document.getElementById('title').value;
+    const extraName = document.getElementById('extra-name').value;
+    const extraValue = document.getElementById('extra-value').value;
+
+    if(title.trim() === '' && extraName.trim() === '' && extraValue.trim() === ''){
+        return;
     }
+
+
+const newMovie = {
+    info: {
+        title,
+        [extraName]: extraValue
+    },
+    id: Math.random()
 };
 
-// Adding, Modifying, & Deleting Properties
-//person.age = 31;
-delete person.age;
-person.isAdmin = true;
+movies.push(newMovie);
+console.log(newMovie);
+};
 
-console.log(person);
-person.greet();
+addMovieBtn.addEventListener('click', addMovieHandler);
+
+
